2afa03dfd9bb279a3c3e960fccbcf590075924e6
miracaltin2004@gmail.com <miracaltin2004@gmail.com>
2021-01-23 23:51:45 +0300

    23 Ocak Ders Sonu

---
+++ OOP3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OOP3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IhtiyacKrediManager ihtiyacKrediManager = new IhtiyacKrediManager();
+            TasitKrediManager tasitKrediManager = new TasitKrediManager();
+            KonutKrediManager konutKrediManager = new KonutKrediManager();
+            EsnafKredisiManager esnafKredisiManager = new EsnafKredisiManager();
+
+            ILoggerService databaseLoggerService = new DatabaseLoggerService();
+            ILoggerService fileLoggerService = new FileLoggerService();
+
+            BasvuruManager basvuruManager = new BasvuruManager();
+            basvuruManager.BasvuruYap(konutKrediManager, fileLoggerService);
+            basvuruManager.BasvuruYap(new EsnafKredisiManager(), new SmsLoggerService());
+
+            List<IKrediManager> krediler = new List<IKrediManager>(){ihtiyacKrediManager};
+            //basvuruManager.KrediOnBilgilendirmesiYap(krediler);
+        }
+    }
+}
